80ebd7500713af2d22a0342a6f7e476fbb28e3e7
IvanDraijer <s1188925@student.windesheim.nl>
2025-05-08 13:04:20 +0200

    lcd done

---
+++ lib/LCD/LCD.h
@@ -0,0 +1,33 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include <stdint.h>
+#include <string.h>
+
+extern "C"
+{
+    #include "i2c_lcd.h"
+}
+
+//comandos for text on screen
+#define CONNECTING 0x01
+#define CONNECTED 0x02
+#define SYNCED 0x03
+#define SYNCING 0x04
+#define AUTO 0x05
+#define MANUAL 0x06
+
+#ifdef __cplusplus
+class LCD
+{
+    public:
+        LCD(); //constructor
+        void printStr(char *str, uint8_t row, uint8_t col); 
+        void sendComannd(uint8_t cmd);
+    protected:
+
+    private:
+};
+#endif // __cplusplus
+
+#endif /* LCD_H */
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+extern "C"
+{
+#include <stdio.h>
+#include "driver/i2c.h"
+#include "LCD.h"
+}
+
+extern "C" void app_main(void)
+{
+    LCD lcd = LCD();              // Clear the LCD screen
+}
