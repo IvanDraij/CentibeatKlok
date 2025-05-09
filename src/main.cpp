4e01a36b3a37e6ad6ec2fff57abfe2224a44d471
LennartGj <lennartgjaltema@gmail.com>
2025-05-09 13:12:11 +0200

    Added second timer for test
    Second timer uses seconds to check if centibeats count faster

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,65 @@ extern "C" {
 }
 
 uint16_t count = 0;
+uint16_t beatCount = 0;
+uint16_t seconds = 0;
 
-void timer_callback(void *param)
+void centibeat_timer_callback(void *param)
 {
-    count++;
+    // if (count >= 99)
+    // {
+    //     beatCount++;
+    //     count = 0;
+    // }
+    // else   
+        count++;
+}
+
+void second_timer_callback(void *param)
+{
+    seconds++;
+}
+
+void centibeat_timer_init(void)
+{
+    const esp_timer_create_args_t centibeat_timer_args = 
+    {
+        .callback = &centibeat_timer_callback,
+        .name = "Centibeat timer Interrupt"
+    };
+
+    esp_timer_handle_t centibeat_timer_handler;
+    esp_timer_create(&centibeat_timer_args, &centibeat_timer_handler);
+    esp_timer_start_periodic(centibeat_timer_handler, 864000);  // One Second = 1000000 micro second
+}
+
+void second_timer_init(void)
+{
+    const esp_timer_create_args_t secondtimer_args = 
+    {
+        .callback = &second_timer_callback,
+        .name = "second timer"
+    };
+    esp_timer_handle_t second_timer_handler;
+    esp_timer_create(&secondtimer_args, &second_timer_handler);
+    esp_timer_start_periodic(second_timer_handler,1000000);
 }
 
 extern "C" void app_main(void)
 {
     LCD lcd = LCD();
-    const esp_timer_create_args_t my_timer_args = 
-    {
-        .callback = &timer_callback,
-        .name = "Timer Interrupt"
-    };
+    centibeat_timer_init();
+    second_timer_init();
 
-    esp_timer_handle_t timer_handler;
-    esp_timer_create(&my_timer_args, &timer_handler);
-    esp_timer_start_periodic(timer_handler, 10000);  // One Second = 1000000 micro second
 
     while(true)
     {
         char ch[8];
         sprintf(ch, "%d", count);
         lcd.printStr(ch, 0,0);
+        char chB[8];
+        sprintf(chB, "%d", seconds);
+        lcd.printStr(chB, 1, 0);
         vTaskDelay(10/ portTICK_PERIOD_MS);
     }
-
 }
