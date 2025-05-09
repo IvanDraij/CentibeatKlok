23fd3e0db04519b063e0163a2b9538945592e0b6
LennartGj <lennartgjaltema@gmail.com>
2025-05-09 14:51:00 +0200

    magic numbers and comments

--- lib/TIMER/TIMER.cpp
+++ lib/TIMER/TIMER.cpp
@@ -1,11 +1,13 @@
 #include "TIMER.h"
+#define amountOfmsForOneCentibeat   864000
+#define hunderBeats                 10000
 
 extern uint16_t count;
 
-void centibeat_timer_callback(void *param)
+void centibeat_timer_callback(void *param) // task to count the centibeats (not an interrupt)
 {
     count++;
-    if (count == 10000) // 100 beats
+    if (count == hunderBeats) // 10000 centibeats
     {
         //NTP CODE
     }
@@ -13,12 +15,12 @@ void centibeat_timer_callback(void *param)
 
 
 
-TIMER::TIMER()
+TIMER::TIMER() // constructor that calls the init function
 {
     TIMER::centibeat_timer_init();
 }
 
-void TIMER::centibeat_timer_init()
+void TIMER::centibeat_timer_init() //initialises the timer for the centibeat
 {
     const esp_timer_create_args_t centibeat_timer_args = 
     {
@@ -28,5 +30,5 @@ void TIMER::centibeat_timer_init()
 
     esp_timer_handle_t centibeat_timer_handler;
     esp_timer_create(&centibeat_timer_args, &centibeat_timer_handler);
-    esp_timer_start_periodic(centibeat_timer_handler, 864000);  // One Second = 1000000 micro second
+    esp_timer_start_periodic(centibeat_timer_handler, amountOfmsForOneCentibeat);  // One Second = 1000000 micro second
 }
