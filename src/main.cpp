664b3118f2a8c195f596384b9dced195c292c4dc
LennartGj <lennartgjaltema@gmail.com>
2025-05-26 15:34:10 +0200

    Added coments

--- src/main.cpp
+++ src/main.cpp
@@ -47,53 +47,53 @@ extern "C" void app_main(void)
     }
 }
 
-void initTasks(LCD *lcd)
+void initTasks(LCD *lcd) // initialises tasks
 {
     initButtonInterrupt();
-    switchButtonSemaphore = xSemaphoreCreateBinary();                 // Create the switchButtonSemaphore
+    switchButtonSemaphore = xSemaphoreCreateBinary();                           // Create the switchButtonSemaphore
     xTaskCreate(vTaskLoopModeButton,"changeModeButton",usStackDepthModeSwitchButton,lcd,modeSwitchButtonPriority,NULL);
 }
 
-void initButtonInterrupt()
+void initButtonInterrupt() // initialises the mode switch button interrupt
 {
     // Configure BUTTON
   gpio_config_t btn_conf = {};
-  btn_conf.intr_type = GPIO_INTR_NEGEDGE;        // Falling edge
-  btn_conf.mode = GPIO_MODE_INPUT;               // Input mode
-  btn_conf.pin_bit_mask = (1ULL << MODE_BUTTON_GPIO); // Which pins need initialising
-  btn_conf.pull_up_en = GPIO_PULLUP_ENABLE;      // Enable pull-up resistor
+  btn_conf.intr_type = GPIO_INTR_NEGEDGE;                                       // Falling edge
+  btn_conf.mode = GPIO_MODE_INPUT;                                              // Input mode
+  btn_conf.pin_bit_mask = (1ULL << MODE_BUTTON_GPIO);                           // Which pins need initialising
+  btn_conf.pull_up_en = GPIO_PULLUP_ENABLE;                                     // Enable pull-up resistor
   gpio_config(&btn_conf);
 
-    gpio_install_isr_service(0); // Install ISR service
+  gpio_install_isr_service(0);                                                  // Install ISR service
 
-    gpio_isr_handler_add(MODE_BUTTON_GPIO, vTaskModeButton, nullptr);
+  gpio_isr_handler_add(MODE_BUTTON_GPIO, vTaskModeButton, nullptr);             // calls vTaskModeButton when interrupt happens
 }
 
-void IRAM_ATTR vTaskModeButton(void *arg)
+void IRAM_ATTR vTaskModeButton(void *arg) // function that is called when button interrupt happens
 {
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    xSemaphoreGiveFromISR(switchButtonSemaphore, &xHigherPriorityTaskWoken);
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;                              // check if higher priority task is running    
+    xSemaphoreGiveFromISR(switchButtonSemaphore, &xHigherPriorityTaskWoken);    // gives semaphore for switchModeButton
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);                               // checks if the semaphore task has higher priority than other tasks
 }
 
 
-void vTaskLoopModeButton(void *arg)
+void vTaskLoopModeButton(void *arg) // loop funtion waiting for switch mode button semaphore
 {
 
     LCD *lcd = static_cast<LCD*>(arg);
 
     while (true)
     {
-        if (xSemaphoreTake(switchButtonSemaphore,portMAX_DELAY))
+        if (xSemaphoreTake(switchButtonSemaphore,portMAX_DELAY))                // wait for semaphore availability
         {
             if (gpio_get_level(MODE_BUTTON_GPIO) == 0)
             {
-                vTaskDelay(pdMS_TO_TICKS(hundredmsDelay));
+                vTaskDelay(pdMS_TO_TICKS(hundredmsDelay));                      // debounce
                 if (gpio_get_level(MODE_BUTTON_GPIO) == 0)
                 {
-                    ESP_LOGI("switchButtonPressed", "switchButton pressed");
-                    automaticMode = !automaticMode;
-                    lcd->sendComannd(automaticMode ? AUTOMODE : MANUAL);
+                    ESP_LOGI("switchButtonPressed", "switchButton pressed");    // serial monitoring
+                    automaticMode = !automaticMode;                             // flips the mode bool
+                    lcd->sendComannd(automaticMode ? AUTOMODE : MANUAL);        // depending on the automaticMode bool the command will be AUTOMODE or MANUAL
 
                 }
             }
